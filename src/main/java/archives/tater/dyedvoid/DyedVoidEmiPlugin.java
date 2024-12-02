132293629941b2d75d7271125505ff013c49c97e
JosiahFu <josiahfu@gmail.com>
2024-12-01 20:33:37 -0800

    Mark emi recipe synthetic

--- src/main/java/archives/tater/dyedvoid/DyedVoidEmiPlugin.java
+++ src/main/java/archives/tater/dyedvoid/DyedVoidEmiPlugin.java
@@ -13,7 +13,7 @@ public class DyedVoidEmiPlugin implements EmiPlugin {
     @Override
     public void register(EmiRegistry registry) {
         registry.addRecipe(EmiWorldInteractionRecipe.builder()
-                        .id(DyedVoid.id("end_void"))
+                        .id(DyedVoid.id("/end_void"))
                         .leftInput(EmiStack.of(DyedVoidItems.BLACK_VOID))
                         .rightInput(EmiIngredient.of(List.of(EmiStack.of(DyedVoidItems.DUMMY_END_PORTAL), EmiStack.of(DyedVoidItems.DUMMY_END_GATEWAY))), true)
                         .output(EmiStack.of(DyedVoidItems.END_VOID))
